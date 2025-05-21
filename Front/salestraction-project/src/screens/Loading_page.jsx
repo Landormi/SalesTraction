d8443b4d2947e38be28f57761279183c2c6edcf3
Houssein372021 <hseinghannoum@gmail.com>
2025-05-21 10:45:07 +0200

    verify otp page

--- Front/salestraction-project/src/screens/Loading_page.jsx
+++ Front/salestraction-project/src/screens/Loading_page.jsx
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
