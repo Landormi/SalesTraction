931b1831422b0da6fdc361693256cc4edb24993f
Houssein372021 <hseinghannoum@gmail.com>
2025-05-23 14:44:51 +0200

    az

--- Front/salestraction-project/src/App.jsx
+++ Front/salestraction-project/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
           <Route path="/profile-talent" element={<ProfileTalent />} />
           <Route path="/offer-talent/:id" element={<DetailsOfferTalent />} />
           <Route path="/contact-talent" element={<ContactTalent/>} />
-          <Route path="/startup-start" element={<StartupSerialNumber/>} />
+
 
 
         </Routes>
